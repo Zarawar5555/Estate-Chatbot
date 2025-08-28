a3b2fff2bae0aa1a548d3618a79970fd66b957ec
Muhammad Zarawar <muhammadzarawar@gmail.com>
2025-08-29 03:41:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1035,7 +1035,7 @@ if __name__ == '__main__':
     logger.info("   - /properties (GET)")
     logger.info("   - /clear-processed (POST)")
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
 
